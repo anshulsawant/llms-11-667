23d59487f00f77067a5fb2f2fea6db153f62090c
Anshul Sawant <anshul.sawant@gmail.com>
2025-01-18 08:31:52 -0800

    Setup for Lambda labs.

---
+++ hw2/setup-everything.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/cmu-llms/miniconda3
+wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/cmu-llms/miniconda3/miniconda.sh
+bash ~/cmu-llms/miniconda3/miniconda.sh -b -u -p /cmu-llms/miniconda3
+rm -rf ~/cmu-llms/miniconda3/miniconda.sh
+~/cmu-llms/miniconda3/bin/conda init bash
+source ~/.bashrc
+conda create --prefix=~/cmu-llms/cmu-11967-hw2 python=3.11
+conda config --append envs_dir ~/cmu-llms
+conda activate cmu-11967-hw2
+pip install -r requirements.txt
+pip install -e .
+wandb login
+curl https://huggingface.co/datasets/yimingzhang/llms-hw2/resolve/main/tokens.npz -o data/tokens.npz -L
