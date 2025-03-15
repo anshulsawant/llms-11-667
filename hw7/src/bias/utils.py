48ac840c1f62e3b47b2392a9ff3c0bb9ce25e081
Anshul Sawant <anshul.sawant@gmail.com>
2025-03-14 18:24:14 -0700

    HW7 coding.

--- hw7/src/bias/utils.py
+++ hw7/src/bias/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from tqdm import tqdm
 import litellm
 import openai
@@ -6,7 +7,9 @@ from datasets import load_dataset
 import os
 from dotenv import load_dotenv
 from typing import List, Tuple, Dict, Union, Any, Optional
-
+from collections import defaultdict
+import torch
+import random
 
 load_dotenv()
 
@@ -34,18 +37,20 @@ def read_data(seed: int, train_size: int, test_size: int) -> Tuple[List[str], Li
     # TODO: Get training and testing datasets from `train` and `validation` splits
     # Step 1: shuffle with seed
     # Step 2: select first train/test size data
-    train_data = ...
-    test_data = ...
+    dataset = dataset.shuffle(seed = seed)
+    train_data = dataset['train'][:train_size]
+    test_data = dataset['validation'][:test_size]
 
     # TODO: Extract sentences and labels
-    train_sentences = ...
-    train_labels = ...
-    test_sentences = ...
-    test_labels = ...
+    train_sentences = train_data['sentence']
+    train_labels = train_data['label']
+    test_sentences = test_data['sentence']
+    test_labels = test_data['label']
 
     return train_sentences, train_labels, test_sentences, test_labels
 
 
+## Example: create_prompt('Input: ', 'Sentiment: ', train_sentences[:4], train_labels[:4], test_sentences[0])
 def create_prompt(
     q_prefix: str,
     a_prefix: str,
@@ -80,6 +85,12 @@ def create_prompt(
         prompt = prompt[:-1]
     
     return prompt
+
+def set_openai_key():
+    with open('/home/anshul/.open_ai_key') as file:
+        key = file.read()
+    os.environ['OPENAI_API_KEY'] = key
+
 
 
 def get_responses(prompts: List[str], echo: bool = False) -> List[Any]:
@@ -150,8 +161,9 @@ def get_label_probs(
     # TODO: Initial probabilities from model responses
     for i, response in enumerate(tqdm(responses, desc="Get initial prob")):
 
-        top_logprobs = ...  # Hint: Check the structure of the response
-        label_probs = ...
+
+        top_logprobs = response.logprobs.top_logprobs  # Hint: Check the structure of the response
+        label_probs = np.zeros((len(label_dict),)) 
 
         for j, label in label_dict.items():
             # add space to match the format
@@ -159,21 +171,21 @@ def get_label_probs(
                 label = " " + label
 
             # Hint: If the label is in the top logprobs, use the probability
-            if label in top_logprobs:
-                label_probs[j] += ...
+            if label in top_logprobs[0]:
+                label_probs[j] += np.exp(top_logprobs[0][label])
             else:
                 # add to missing positions
-                ...
-
+                all_missing_positions.append((i, j))
         all_label_probs.append(label_probs)
-    all_label_probs = np.array(all_label_probs)
 
+    all_label_probs = np.array(all_label_probs)
     # TODO: Fill in those missing positions
     all_additional_prompts = []
 
     for i, j in all_missing_positions:
         # Hint: Based on the index, use create_prompt to create a new prompt for the missing position
-        prompt = ...
+        label = label_dict[j]
+        prompt = create_prompt('Input: ', 'Sentiment: ', few_shot_sentences, few_shot_labels, test_sentences[i])
 
         # add space to match the format
         # Hint: It's important to understand why we append the label to the input prompt
@@ -188,7 +200,7 @@ def get_label_probs(
     for idx, (i, j) in enumerate(all_missing_positions):
         response = additional_responses[idx]
         # TODO: Get the probability from the response
-        prob = ...
+        prob = np.exp(response.logprobs.token_logprobs[-1])
         all_label_probs[i][j] = prob
 
     return all_label_probs  # this is not normalized
@@ -219,7 +231,7 @@ def calibrate(
     num_labels = len(label_dict)
 
     # TODO: Create a prompt with content-free input
-    prompt = ...
+    prompt = create_prompt(q_prefix , a_prefix, few_shot_sentences, few_shot_labels, content_free_input)
     p_y = [0] * num_labels
 
     for i, answer in label_dict.items():
@@ -231,10 +243,10 @@ def calibrate(
         response = get_responses(prompts=[prompt+key], echo=True)[0]
 
         # TODO: Get the probability from the response
-        p_y[i] = ...
-
+        p_y[i] = np.exp(response.logprobs.token_logprobs[-1] )
+        
     # TODO: Normalize the probabilities
-    p_y = ...
+    p_y = p_y/sum(p_y)
 
     return p_y
 
@@ -258,17 +270,38 @@ def eval_accuracy(all_label_probs: np.ndarray, test_labels: List[int], p_cf: Opt
 
     # TODO: Initialize W and b
     if p_cf is None:
-        W = ...
-        b = ...
+        W = np.identity(all_label_probs.shape[1])
+        b = 0
     else:
-        W = ...
-        b = ...
+        W = np.diag(1/p_cf)
+        b = 0
 
     # TODO: Calculate the accuracy
     corrects = []
     for prob, label in zip(all_label_probs, test_labels):
-        ...
+        prob = torch.nn.functional.softmax(torch.from_numpy(W@prob) + b)
+        corrects.append(np.argmax(prob) == label)
 
     accuracy = np.mean(corrects)
     
     return accuracy
+
+
+def before_and_after(n = 2, N=100, seed=42):
+    random.seed(seed)
+    train_sentences, train_labels, test_sentences, test_labels = read_data(seed, 1000, N)
+    train_len = len(train_sentences)
+    q_prefix = 'Input: '
+    a_prefix = 'Sentiment: '
+    cf = 'N/A'
+    idx = random.sample(range(train_len), n)
+    s = [train_sentences[j] for j in idx]
+    l = [train_labels[j] for j in idx]
+    p_y = calibrate(cf, s, l, q_prefix=q_prefix, a_prefix=a_prefix) 
+    test_responses = get_responses([create_prompt(q_prefix, a_prefix, s, l, t) for t in test_sentences])
+    all_label_probs = get_label_probs(test_responses, s, l, test_sentences, q_prefix, a_prefix)
+    print(all_label_probs)
+    print(test_labels)
+    acc = eval_accuracy(all_label_probs, test_labels)
+    acc_calibrated = eval_accuracy(all_label_probs, test_labels, p_y)
+    print(f'Original accuracy: {acc}, Calibrated accuracy {acc_calibrated}')
